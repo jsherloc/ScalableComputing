c5d7cfaf269cb3d525d1d78201113edafea0bbcc
James Sherlock <jamessherlock@Jamess-MacBook-Pro.local>
2018-10-12 19:20:51 +0100

    Adding some rules.

--- pracFour/setupPracFour.sh
+++ pracFour/setupPracFour.sh
@@ -4,8 +4,6 @@ sudo apt update
 sudo apt upgrade
 sudo apt-get install build-essential
 sudo apt-get install linux-image-extra-virtual
-echo "Running the driver install...";
-sudo /bin/bash NVIDIA-Linux-x86_64-367.128.run
 echo "Install Hashcat...";
 sudo apt install hashcat
 echo "Let's see does Hashcat see the GPUs...";
